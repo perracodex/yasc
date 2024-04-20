1e88b0a86ba43d8e8fda2900659d0461d7e79b0e
Fernando Sanchez <perracolabs@gmail.com>
2024-04-20 09:28:46 +0200

    Updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,33 +60,36 @@ dependencies {
     // JetPack/AndroidX dependencies.
     // https://developer.android.com/jetpack/androidx/releases
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Material Design.
     implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.compose.material3:material3:1.2.1")
 
     // Compose.
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material3:material3:1.2.1")
-    implementation("androidx.compose.ui:ui:1.6.3")
-    implementation("androidx.compose.ui:ui-graphics:1.6.3")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.6.3")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.6.3")
-    implementation("androidx.compose.ui:ui-graphics:1.6.3")
+    val composeVersion = "1.6.6"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
 
     // Compose navigation.
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // LiveData.
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    val lifecycleVersion = "2.7.0"
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycleVersion")
 
     // Kotlin's coroutines.
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    val coroutinesVersion = "1.8.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutinesVersion")
 
     // Kotlin's serialization. Used for different serialization tasks,
     // for example when de-serializing entities retrieved from a remote data source.
@@ -94,8 +97,9 @@ dependencies {
 
     // Dependency injection dependencies.
     // https://developer.android.com/training/dependency-injection/hilt-android
-    implementation("com.google.dagger:hilt-android:2.51")
-    kapt("com.google.dagger:hilt-android-compiler:2.51")
+    val hiltVersion = "2.51"
+    implementation("com.google.dagger:hilt-android:$hiltVersion")
+    kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
     // Retrofit.
     implementation("com.squareup.retrofit2:retrofit:2.10.0")
@@ -104,7 +108,7 @@ dependencies {
 
     // Datastore.
     // https://developer.android.com/topic/libraries/architecture/datastore
-    implementation("androidx.datastore:datastore-preferences:1.1.0-beta02")
+    implementation("androidx.datastore:datastore-preferences:1.1.0")
 
     // Unit testing.
     testImplementation("junit:junit:4.13.2")
